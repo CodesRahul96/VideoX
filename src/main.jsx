9c988262bfd06548ad25eff3d3e5e6431dcc98bd
CodesRahul <codesrahul96@gmail.com>
2024-12-09 14:00:20 +0530

    updated api

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.scss";
 import { store } from "./store/store.js";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
